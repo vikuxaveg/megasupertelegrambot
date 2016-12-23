5a37e024d724c07675636fe5e7a849699654d9e6
vikuxaveg <vikuxaveg@fulvie.com>
2016-12-23 20:20:40 +0000

    x

--- bot.py
+++ bot.py
@@ -1,1 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import re
 import telebot
+import urllib2
+token = '323152778:AAEEm2guy91BDoM32qwgp3dAON0WTFm0fK8'
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+        bot.reply_to(message, u"Привет, как дела?")
+
+
+@bot.message_handler(regexp="give me (.*)")
+def handle_message(message):
+    data = re.search("give me (.*)", message.text).group(1)
+    x = urllib2.urlopen('http://hh.ru').read()
+    res = re.search(".*href=\"(.*?\.jpg)\".*?>",x)
+    if res is not None:
+        img = "https://lurkmore.to"+res.group(1)
+        bot.send_message(message.chat.id, img)
+    else:
+        bot.send_message(message.chat.id, u"Я не смог")
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    if message.text == u"кис":
+        bot.send_message(message.chat.id, u"мяу")
+    else:
+        bot.send_message(message.chat.id, message.text)
+
+
+bot.polling()
